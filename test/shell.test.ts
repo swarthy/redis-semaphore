bcf9b398872c1ea740676e089a32451cd0de8f7b
Alexander Mochalin <horroshow@mail.ru>
2021-04-14 12:45:08 +0500

    test: increase timeout for up/shutdown redis tests

--- test/shell.test.ts
+++ test/shell.test.ts
@@ -2,10 +2,12 @@ import { downRedisServer, upRedisServer } from './shell'
 
 describe('TEST UTILS', () => {
   describe('shell', () => {
-    it('should up redis server', async () => {
+    it('should up redis server', async function () {
+      this.timeout(30000)
       await upRedisServer(1)
     })
-    it('should down and up redis servers', async () => {
+    it('should down and up redis servers', async function () {
+      this.timeout(30000)
       await downRedisServer(1)
       await upRedisServer(1)
     })
